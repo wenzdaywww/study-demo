fa37d454351ad296af818eff80b74c6e541a44b6
wenzdaywww <289209496@qq.com>
2021-06-25 23:46:36 +0800

    集成swagger框架

--- src/main/java/com/www/demo/swagger/config/Swagger2Config.java
+++ src/main/java/com/www/demo/swagger/config/Swagger2Config.java
@@ -1,12 +1,15 @@
 package com.www.demo.swagger.config;
 
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author www
  * @version 1.0
  * @description swagger配置类
  * @date 2021/6/24 23:19
  */
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class Swagger2Config {
 }
