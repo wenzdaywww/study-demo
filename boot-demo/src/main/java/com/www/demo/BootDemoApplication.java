8df484bc48993503f401db74529f3920bb40ad1d
wenzdaywww <289209496@qq.com>
2021-07-26 22:01:47 +0800

    实现dubbo+zookeeper

--- boot-demo/src/main/java/com/www/demo/BootDemoApplication.java
+++ boot-demo/src/main/java/com/www/demo/BootDemoApplication.java
@@ -1,5 +1,6 @@
 package com.www.demo;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication //springboot核心注解，主要用于开启spring自动配置
 @MapperScan(basePackages = "com.www.demo.model.mapper")//配置mapper扫描路径。mapper接口类不使用@Mapper时使用该注解
-//@EnableDubboConfig//开启dubbo注解
+@EnableDubbo //开启dubbo注解
 public class BootDemoApplication {
 	/*
 	 * @Author www
