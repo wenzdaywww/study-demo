b9c70ff9ed75130563f3259f77a452ab405de93e
wenzdaywww <289209496@qq.com>
2021-08-01 21:32:31 +0800

    完善注释

--- boot-demo/src/main/java/com/www/demo/app/service/ISysUserService.java
+++ boot-demo/src/main/java/com/www/demo/app/service/ISysUserService.java
@@ -13,45 +13,42 @@ import java.util.List;
  */
 public interface ISysUserService {
     /**
-     * @Author www
-     * @Date 2021/6/15 23:15
-     * @Description 查询用户信息，包含角色信息
-     *
+     * <p>@Description 查询用户信息，包含角色信息 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2021/8/1 21:30 </p>
      * @param userId 用户id
-     * @return com.www.demo.model.entity.SysUserEntity
+     * @return com.www.demo.model.dto.SysUserDTO
      */
     SysUserDTO findUserAllInfo(String userId);
     /**
-     * @Author www
-     * @Date 2021/6/7 22:56
-     * @Description 查询用户信息
+     * <p>@Description 查询用户信息 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2021/8/1 21:30 </p>
      * @param page 当前页数
      * @param user 查询条件
-     * @return java.util.List<com.www.demo.model.entity.SysUserEntity>
+     * @return java.util.List<com.www.demo.model.entity.SysUser>
      */
     List<SysUser> findUserList(int page,SysUser user);
     /**
-     * <p>@description 根据用户ID查询用户信息</p>
-     * <p>@author www </p>
-     * <p>@date 2021/8/1 20:30 </p>
+     * <p>@Description 根据用户ID查询用户信息</p>
+     * <p>@Author www </p>
+     * <p>@Date 2021/8/1 20:30 </p>
      * @param userId 用户ID
      * @return com.www.demo.model.entity.SysUser 用户信息
      */
     SysUser selectByUserId(String userId);
     /**
-     * @Author www
-     * @Date 2021/5/19 23:41
-     * @Description 插入用户信息（更新非空数据）
-     *
+     * <p>@Description 插入用户信息（更新非空数据） </p>
+     * <p>@Author www </p>
+     * <p>@Date . 21:31 </p>
      * @param record 用户信息
      * @return int 插入条数
      */
     int insertSelective(SysUser record);
     /**
-     * @Author www
-     * @Date 2021/5/19 23:42
-     * @Description 根据主键更新用户信息（更新非空数据）
-     *
+     * <p>@Description 根据主键更新用户信息（更新非空数据） </p>
+     * <p>@Author www </p>
+     * <p>@Date 2021/8/1 21:31 </p>
      * @param record 用户信息
      * @return int 插入条数
      */
