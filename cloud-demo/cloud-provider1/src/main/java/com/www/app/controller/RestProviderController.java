7874623ddb74ce02f061318f1f50725d619b53c0
wenzdaywww <289209496@qq.com>
2021-08-08 16:10:00 +0800

    集成hystrix熔断机制

--- cloud-demo/cloud-provider1/src/main/java/com/www/app/controller/RestProviderController.java
+++ cloud-demo/cloud-provider1/src/main/java/com/www/app/controller/RestProviderController.java
@@ -1,7 +1,9 @@
 package com.www.app.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.www.data.common.ResponseDTO;
 import com.www.data.dto.SysUserDTO;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>@Description Restful API提供方
- *  * @date 2021/8/1 18:02 </p>
  * <p>@Version 1.0 </p>
  * <p>@Author www </p>
  * <p>@Date 2021/8/1 21:23 </p>
@@ -25,14 +26,33 @@ public class RestProviderController {
     @Value("${server.port}")
     private String port;
 
+    @HystrixCommand(fallbackMethod = "hystrixFindName")//设置备选方案
     @GetMapping("/get/{name}")
     public ResponseDTO findName(@PathVariable("name") String name){
         LOGGER.info("----> /pro/get/{name}的name={}",name);
+        if(StringUtils.equals(name,"hystrix")){
+            //此处抛出异常，则会执行备选方法hystrixFindName
+            throw  new RuntimeException("----> hystrix test");
+        }
         SysUserDTO sysUserDTO = new SysUserDTO();
         sysUserDTO.setUserName(name);
         sysUserDTO.setUserId(port);
         ResponseDTO<SysUserDTO> responseDTO = new ResponseDTO<SysUserDTO>(sysUserDTO);
         return responseDTO;
     }
-
+    /**
+     * <p>@Description /get/{name}失败的备选方案 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2021/8/8 15:33 </p>
+     * @param name
+     * @return com.www.data.common.ResponseDTO
+     */
+    public ResponseDTO hystrixFindName(@PathVariable("name") String name){
+        LOGGER.info("----> /get/{name}失败的备选方案的name={}",name);
+        SysUserDTO sysUserDTO = new SysUserDTO();
+        sysUserDTO.setUserName("/get/{name}失败的备选方案的name="+name);
+        sysUserDTO.setUserId(port);
+        ResponseDTO<SysUserDTO> responseDTO = new ResponseDTO<SysUserDTO>(sysUserDTO);
+        return responseDTO;
+    }
 }
