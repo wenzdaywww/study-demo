cf00b1ac8be8e00d3392b79a68eccd9d67f49186
wenzdaywww <289209496@qq.com>
2021-08-28 17:21:53 +0800

    rabbitmq的死信队列

--- rabbitmq-demo/src/main/java/com/www/rabbitmq/config/DeadQueueConfig.java
+++ rabbitmq-demo/src/main/java/com/www/rabbitmq/config/DeadQueueConfig.java
@@ -7,17 +7,14 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * <p>@Description rabbitmq配置类，主要配置队列与交换区关系，如果已在管理页面配置则不需要再配置 </p>
+ * <p>@Description rabbitmq配置类，死信队列配置 </p>
  * <p>@Version 1.0 </p>
  * <p>@Author www </p>
  * <p>@Date 2021/8/26 21:57 </p>
  */
 @Configuration
-public class TestQueueConfig {
+public class DeadQueueConfig {
     /**
      * <p>@Description 配置交换区 </p>
      * <p>@Author www </p>
@@ -25,8 +22,8 @@ public class TestQueueConfig {
      * @return org.springframework.amqp.core.DirectExchange
      */
     @Bean
-    public DirectExchange testDirect(){
-        return new DirectExchange("test-direct",true,false);
+    public DirectExchange deadDirect(){
+        return new DirectExchange("dead-direct",true,false);
     }
     /**
      * <p>@Description 配置队列 </p>
@@ -35,22 +32,17 @@ public class TestQueueConfig {
      * @return org.springframework.amqp.core.Queue
      */
     @Bean
-    public Queue testQueue(){
-        Map<String, Object> arguments = new HashMap<>();
-        arguments.put("x-message-ttl",10000); //设置过期时间
-        arguments.put("x-dead-letter-exchange","dead-direct"); //设置死信队列
-        arguments.put("x-dead-letter-routing-key","dead"); //设置死信队列key
-        return new Queue("test-queue",true,false,false,arguments);
+    public Queue deadQueue(){
+        return new Queue("dead-queue",true,false,false);
     }
     /**
      * <p>@Description 绑定交换区与队列 </p>
      * <p>@Author www </p>
      * <p>@Date 2021/8/26 22:01 </p>
-     *
      * @return org.springframework.amqp.core.Binding
      */
     @Bean
-    public Binding testBinding(){
-        return BindingBuilder.bind(testQueue()).to(testDirect()).with("test");
+    public Binding deadBinding(){
+        return BindingBuilder.bind(deadQueue()).to(deadDirect()).with("dead");
     }
 }
